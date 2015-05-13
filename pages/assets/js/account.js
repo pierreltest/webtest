c82d05f6dba26139dac24cb104d0994504c1996f
Pierre Larochelle <pierrebombay@gmail.com>
2015-05-13 12:52:02 -0700

    Moving to camel case

--- pages/assets/js/account.js
+++ pages/assets/js/account.js
@@ -11,10 +11,10 @@ Account = (function() {
   }
 
   Account.prototype.render = function(targetEl) {
-    var is_url_element = function(account_element) { return account_element.type == 'url'; };
-    var is_image_element = function(account_element) { return account_element.type == 'url_with_css_image_hash'; };
-    var is_search_element = function(account_element) { return account_element.type == 'search'; };
-    var social_element_type = function(klass) {
+    var isUrlElement = function(accountElement) { return accountElement.type == 'url'; };
+    var isImageElement = function(accountElement) { return accountElement.type == 'url_with_css_image_hash'; };
+    var isSearchElement = function(accountElement) { return accountElement.type == 'search'; };
+    var socialElementType = function(klass) {
       if (klass == 'facebook-img') {
         return 'facebook';
       } else if (klass == 'google-plus-img') {
@@ -23,47 +23,46 @@ Account = (function() {
         return 'twitter';
       }
     }
-    var url_menu_element = function(url, text) {
+    var urlMenuElement = function(url, text) {
       return '<li class="utility__list-item  utility__list-item--show-lg utility__account-menu-item js-utility__account-menu-item"><a class="utility__cta" href="' + url + '">' + text + '</a></li>';
     }
-    var image_menu_element = function(klass, url) {
-      var type = social_element_type(klass);
+    var imageMenuElement = function(klass, url) {
+      var type = socialElementType(klass);
       return '<li class="utility__list-item  utility__list-item--social  utility__list-item--show-lg"><a class="utility__cta" href="' + url + '"><span class="icon  social--utility--' + type  + '--blue"></span></a></li>';
     }
-    var menu_element = function(account_element) {
-      if (is_url_element(account_element)) {
-        return url_menu_element(account_element.url, account_element.text);
-      } else if (is_image_element(account_element)) {
-        return image_menu_element(account_element.class, account_element.url);
+    var menuElement = function(accountElement) {
+      if (isUrlElement(accountElement)) {
+        return urlMenuElement(accountElement.url, accountElement.text);
+      } else if (isImageElement(accountElement)) {
+        return imageMenuElement(accountElement.class, accountElement.url);
       } else {
         return '';
       }
     }
-    var url_menu_sum = function(memo, elem) { return memo + elem; };
+    var urlMenuSum = function(memo, elem) { return memo + elem; };
 
-    var update_search_url = function(url) {
+    var updateSearchUrl = function(url) {
       $(targetEl + ' .utility__list-item--search form').attr('action', url);
     }
 
-    var update_account_menu = function(account_menu) {
+    var updateAccountMenu = function(accountMenu) {
       // add the account stuff
       var $list = $(targetEl + ' ul'); 
-      var forward_elements = _.chain(account_menu)
-          .filter(is_url_element)
-          .map(menu_element)
-          .reduce(url_menu_sum, '')
+      var forwardElements = _.chain(accountMenu)
+          .filter(isUrlElement)
+          .map(menuElement)
+          .reduce(urlMenuSum, '')
           .value();
-      var backward_elements = _.chain(account_menu)
-          .filter(is_image_element)
-          .map(menu_element)
-          .reduce(url_menu_sum, '')
+      var backwardElements = _.chain(accountMenu)
+          .filter(isImageElement)
+          .map(menuElement)
+          .reduce(urlMenuSum, '')
           .value();
-      $list.prepend(forward_elements);
-      $list.append(backward_elements);
-      
-      // update the search url
-      var search_url = _.find(account_menu, is_search_element).url;
-      update_search_url(search_url);
+      var searchUrl = _.find(accountMenu, isSearchElement).url;
+
+      $list.prepend(forwardElements);
+      $list.append(backwardElements);
+      updateSearchUrl(searchUrl);
     }
 
     $.ajax({
@@ -71,7 +70,7 @@ Account = (function() {
     })
       .done(function(resp) {
         if (resp.account_menu) {
-          update_account_menu(resp.account_menu);
+          updateAccountMenu(resp.account_menu);
         } else {
           console.log('something is wrong with the account menu response');
         }
