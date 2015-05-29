ae2faa92ccc0adaabcea976b99b77fbc42d0de91
Pierre Larochelle <plarochelle@blurb.com>
2015-05-29 12:53:25 -0700

    Adding generator to move fonts

--- _plugins/ext.rb
+++ _plugins/ext.rb
@@ -1,1 +1,12 @@
 require "jekyll-assets"
+
+module Blurb
+  class BuildkitFontGenerator < Jekyll::Generator
+    safe true
+    
+    def generate(site)
+      `rm -rf _site/fonts`
+      `cp -R _site/assets/bower/buildkit/assets/fonts _site/fonts`
+    end
+  end
+end
