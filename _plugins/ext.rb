7a641e36a0cfb8922c8d0a8f843349c8976b7369
Pierre Larochelle <plarochelle@blurb.com>
2015-05-29 15:58:01 -0700

    moving compiled assets to new asset location

--- _plugins/ext.rb
+++ _plugins/ext.rb
@@ -7,6 +7,7 @@ module Blurb
     def generate(site)
       `rm -rf _site/fonts`
       `cp -R pages/new-website-assets/bower/buildkit/assets/fonts _site/fonts`
+      `cp _site/assets/* _site/new-website-assets/ 2>/dev/null`
     end
   end
 end
