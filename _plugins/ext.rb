18433382e684493fab5dc6a8b754184bd1bbb89a
Pierre Larochelle <plarochelle@blurb.com>
2015-05-29 17:00:59 -0700

    Making it possible to use sed on unix

--- _plugins/ext.rb
+++ _plugins/ext.rb
@@ -5,8 +5,9 @@ module Blurb
     safe true
     
     def generate(site)
+      mac = RbConfig::CONFIG['host_os'].match(/darwin/)
       `cp _site/assets/* _site/new-website-assets/ 2>/dev/null`
-      `sed -i '' 's/url\("../url\("\\/new-website-assets\\/bower\\/buildkit\\/assets/g' _site/new-website-assets/*.css`
+      `sed -i #{ mac ? "''" : ''} 's/url\("../url\("\\/new-website-assets\\/bower\\/buildkit\\/assets/g' _site/new-website-assets/*.css`
     end
   end
 end
