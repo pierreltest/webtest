8dc2c7524111a1b36b9405ecf05502a07565bc62
Pierre Larochelle <plarochelle@blurb.com>
2015-06-01 11:11:33 -0700

    Using latest buildkit with only assets included

--- _plugins/ext.rb
+++ _plugins/ext.rb
@@ -7,7 +7,7 @@ module Blurb
     def generate(site)
       mac = RbConfig::CONFIG['host_os'].match(/darwin/)
       `cp _site/assets/* _site/new-website-assets/ 2>/dev/null`
-      `sed -i #{ mac ? "''" : ''} 's/url\("../url\("\\/new-website-assets\\/bower\\/buildkit\\/assets/g' _site/new-website-assets/*.css`
+      `sed -i #{ mac ? "''" : ''} 's/url\("../url\("\\/new-website-assets\\/bower\\/buildkit/g' _site/new-website-assets/*.css`
     end
   end
 end
